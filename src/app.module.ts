8472bacc691cd770280426df4146b8f5f76a3885
Matheus Neto Melo <matheusmelo180203@gmail.com>
2025-08-27 18:53:40 -0300

    feat: create module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ModuleModule } from './module/module.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    ModuleModule,
   ],
 })
 export class AppModule {}
